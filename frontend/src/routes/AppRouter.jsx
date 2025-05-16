c9eaa6d252008702733806915ad460a6879e3847
Illya <archasmiel@gmail.com>
2025-05-16 19:24:20 +0300

    /dashboard to privateRoute

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from '../features/auth/pages/AuthPage';
+import PrivateRoute from './PrivateRoute';
 
 const Dashboard = () => <div>Welcome</div>;
 
 const AppRouter = () => (
   <Router>
     <Routes>
-      <Route path="/auth/" element={<AuthPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </Router>
 );
