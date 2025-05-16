d82b282fe34ca0f4067736dad71dd7d2b702f41d
Illya <archasmiel@gmail.com>
2025-05-16 23:26:17 +0300

    add api.js in constants

---
+++ frontend/src/shared/constants/api.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = 'http://localhost:8080/api';
+export const AUTH_ENDPOINTS = {
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/signup',
+  LOGOUT: '/auth/logout',
+};
+export const API_HEADERS = {
+  CONTENT_TYPE: 'application/json',
+  AUTHORIZATION: 'Authorization',
+};
