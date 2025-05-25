a0b1008a9ed2dcf175191fb4809e2f471fc35d21
Illya <illakoroluk62@gmail.com>
2025-05-25 09:41:02 +0300

    [Refact] errorInteceptor

--- frontend/src/api/interceptors/ErrorInterceptor.js
+++ frontend/src/api/interceptors/ErrorInterceptor.js
@@ -1,27 +1,16 @@
-class ErrorInterceptor {
+import TokenService from '../../features/auth/utils/TokenService';
+
+export class ErrorInterceptor {
   static setup(axiosInstance) {
     axiosInstance.interceptors.response.use(
-      (response) => response,
-      (error) => {
-        if (error.response) {
-          switch (error.response.status) {
-            case 401:
-              console.error('Unauthorized access');
-              break;
-            case 403:
-              console.error('Forbidden access');
-              break;
-            case 500:
-              console.error('Server error');
-              break;
-            default:
-              console.error('Request error', error.message);
-          }
+      response => response,
+      error => {
+        if (error.response?.status === 401) {
+          TokenService.clearToken();
+          window.location.href = '/auth';
         }
         return Promise.reject(error);
       }
     );
   }
 }
-
-export default ErrorInterceptor;
