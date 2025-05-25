526a952e742c9f24dc32ad80236e8f0b749ec689
Illya <illakoroluk62@gmail.com>
2025-05-25 10:04:32 +0300

    [Refact] PublicRoute

--- frontend/src/routes/PublicRoute.jsx
+++ frontend/src/routes/PublicRoute.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import useAuth from '../app/hooks/useAuth';
+import { useAuth } from '../app/hooks/useAuth';
 
-const PublicRoute = () => {
+export const PublicRoute = () => {
   const { isAuthenticated } = useAuth();
   return !isAuthenticated ? <Outlet /> : <Navigate to="/dashboard" replace />;
 };
-
-export default PublicRoute;
