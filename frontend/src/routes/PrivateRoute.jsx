fb6e1d80535268af1ad88de8c9aa09f5f241d24b
Illya <illakoroluk62@gmail.com>
2025-05-25 10:04:19 +0300

    [Refact] Private Route

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import useAuth from '../app/hooks/useAuth';
+import { useAuth } from '../app/hooks/useAuth';
 
-const PrivateRoute = () => {
+export const PrivateRoute = () => {
   const { isAuthenticated } = useAuth();
   return isAuthenticated ? <Outlet /> : <Navigate to="/auth" replace />;
 };
-
-export default PrivateRoute;
