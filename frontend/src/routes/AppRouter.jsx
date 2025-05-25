9a4960f4417c9503e0cef4d52f78c9fe0fe8427a
Illya <illakoroluk62@gmail.com>
2025-05-25 10:04:00 +0300

    [Refact] AppRouter

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
-import AuthPage from '../features/auth/pages/AuthPage/AuthPage';
-import DashboardPage from '../features/auth/pages/DashboardPage/DashboardPage';
-import { Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthPage } from '../features/auth/pages/AuthPage/AuthPage';
+import { DashboardPage } from '../features/auth/pages/DashboardPage/DashboardPage';
 
-const AppRouter = () => (
+export const AppRouter = () => (
   <Routes>
     <Route element={<PublicRoute />}>
       <Route path="/auth" element={<AuthPage />} />
     </Route>
-    
+
     <Route element={<PrivateRoute />}>
       <Route path="/dashboard" element={<DashboardPage />} />
     </Route>
-    
-    <Route path="*" element={<Navigate to="/dashboard" replace />} />
+
+    <Route path="*" element={<Navigate to="/auth" replace />} />
   </Routes>
-
 );
-
-export default AppRouter;
